97ddbe5882e971ad068e67353ca1077b284348ac
adriennette <92022862+adriennette@users.noreply.github.com>
2021-12-13 19:55:27 +0530

    Add files via upload

---
+++ 170 class.py
@@ -0,0 +1,29 @@
+class Bangkok():
+    def year_2018(self):
+        print("\n","Ice-skating in bangkok","\n")
+        print("In 2018 I won a Ice-skating compitiion in bankok, I was 9 years okd and i won 3 gold medals")
+             
+    def year_2019(self):
+        print("\n","Event in 2019 neltas","\n")
+       
+class Neltas():
+    def year_2019(self):
+        print("\n","Event in 2019","\n")
+        print("I won the merit in 2019 neltas. I got 92% in my exam")
+    
+       
+
+       
+Ice = Bangkok()
+Ice.year_2018()
+Ice.year_2019()
+
+obj_neltas = Neltas()
+obj_neltas.year_2019
+
+"""
+Created on Mon Dec 13 19:33:37 2021
+
+@author: Admin
+"""
+
